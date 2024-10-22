5efc668f29d7f8fd0eb0e365be4314293122d979
Iqra-4 <iqraktanoli@gmail.com>
2024-10-22 23:32:46 +1100

    Update index.js function name

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ let apiKey = "25003fb5bf42fe9747o6t953e32a77ae";
 let apiUrl =
   `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
 
-function displayTemperature (response) {
+function showTemperature (response) {
   console.log(response.data.temperature.current);
   let temperatureElement = document.querySelector('#temperature');
   temperatureElement.innerHTML = response.data.temperature.current;
 }
 
-axios.get(apiUrl).then(displayTemperature);
+axios.get(apiUrl).then(showTemperature);
